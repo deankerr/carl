20c215f7b3dc0f0d6318f0a0a38ed1f092bd1bd5
Dean Kerr <dean.kerr@gmail.com>
2022-12-30 23:30:27 +1100

    note

--- src/Components.ts
+++ src/Components.ts
@@ -1,3 +1,5 @@
+// ? Integrate builder - functions could take optional object of components
+// ? causing them to add themselves to that and return "this" ?
 export type Render = { render: { char: string; color: string } }
 export type Position = { position: { x: number; y: number } }
 export type TagPlayer = { tagPlayer: true }
@@ -5,7 +7,10 @@ export type TagPlayer = { tagPlayer: true }
 export type ID = {
   id: string
 }
+
+// currently needed only for updateComponents on world, I probably don't want this
 export type ComponentsU = Position | Render | TagPlayer
+
 export type Components = Partial<Position & Render & TagPlayer>
 export type Entity = ID & Components
 
