e334546834a65702656f2c67ac3bb919d7c86a4a
Dean Kerr <dean.kerr@gmail.com>
2023-01-10 14:50:28 +1100

    handleMovement uses Point

--- src/System/handleMovement.ts
+++ src/System/handleMovement.ts
@@ -10,55 +10,53 @@ export const handleMovement = (world: World) => {
 
   const action = currentEntity.acting
 
-  if ('move' in action) {
-    console.log('handleMovement:', currentEntity.id, action.move)
-    const currentIsPlayer = 'tagPlayer' in currentEntity
-
-    // wait, just return (for now)
-    if (action.move.dir === 'WAIT') {
-      console.log('handleMovement: result - wait')
-      return
-    }
-
-    const { position: oldPosition } = currentEntity
-
-    const newX = oldPosition.x + action.move.dx
-    const newY = oldPosition.y + action.move.dy
-
-    if (world.options.debugMode && currentIsPlayer) {
-      // update position
-      world.modify(currentEntity).change(position(Pt(newX, newY)))
-      return
-    }
-
-    // if null (out of bounds) act like its a wall
-    const terrain = world.state.level.terrain.get(Pt(newX, newY)) ?? 1
-
-    // terrain walkable check
-    if (!TerrainDictionary[terrain].walkable) {
-      console.log('handleMovement: new action - Bump (terrain)')
-      const newAction = acting(Bump(Pt(newX, newY)))
-      world.modify(currentEntity).change(newAction)
-      return
-    }
-
-    // entity blocking check
-    const here = world.here(Pt(newX, newY))
-    const entitiesWalkable = here[1].every(e => 'tagCurrentTurn' in e || 'tagWalkable' in e)
-    if (!entitiesWalkable) {
-      console.log('handleMovement: new action - Bump (entity)')
-      const newAction = acting(Bump(Pt(newX, newY)))
-      world.modify(currentEntity).change(newAction)
-      return
-    } else {
-      // create tread action
-      console.log('handleMovement: new action - Tread')
-      const tread = acting(Tread(Pt(newX, newY)))
-      // update position
-      const pos = position(Pt(newX, newY))
-      world.modify(currentEntity).change(tread).change(pos)
-    }
-  } else {
+  if (!('move' in action)) {
     console.log('handleMovement: not a move action', action)
+    return
+  }
+
+  console.log('handleMovement:', currentEntity.id, action.move)
+  const currentIsPlayer = 'tagPlayer' in currentEntity
+
+  // wait, just return (for now)
+  if (action.move.dir === 'WAIT') {
+    console.log('handleMovement: result - wait')
+    return
+  }
+
+  const newPt = Pt(currentEntity.position.x + action.move.dx, currentEntity.position.y + action.move.dy)
+
+  // debug no clip
+  if (world.options.debugMode && currentIsPlayer) {
+    world.modify(currentEntity).change(position(newPt))
+    return
+  }
+
+  // if null (out of bounds) act like its a wall
+  const terrain = world.state.level.terrain.get(newPt) ?? 1
+
+  // terrain walkable check
+  if (!TerrainDictionary[terrain].walkable) {
+    console.log('handleMovement: new action - Bump (terrain)')
+    const newAction = acting(Bump(newPt))
+    world.modify(currentEntity).change(newAction)
+    return
+  }
+
+  // entity blocking check
+  const here = world.here(newPt)
+  const entitiesWalkable = here[1].every(e => 'tagCurrentTurn' in e || 'tagWalkable' in e)
+  if (!entitiesWalkable) {
+    console.log('handleMovement: new action - Bump (entity)')
+    const newAction = acting(Bump(newPt))
+    world.modify(currentEntity).change(newAction)
+    return
+  } else {
+    // create tread action
+    console.log('handleMovement: new action - Tread')
+    const tread = acting(Tread(newPt))
+    // update position
+    const newPosition = position(newPt)
+    world.modify(currentEntity).change(tread).change(newPosition)
   }
 }
