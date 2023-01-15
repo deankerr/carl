cd419c2b65ea46d47dd9e8ae4d03729e1955237e
Dean Kerr <dean.kerr@gmail.com>
2023-01-16 02:49:50 +1100

    Remove dungeon4/visualizer init from App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,110 +1,22 @@
 // Initial set up/loading test modules
-// TODO use window.gameSeed = '111' / localStorage
 import { CONFIG } from './config'
 import { createGameDisplay } from './lib/display'
 import * as ROT from 'rot-js'
-import { Keys } from './lib/Keys'
 
-let display: ROT.Display
-let msgDisplay: ROT.Display
-const keys: Keys = new Keys()
-
-// game
 import { Game } from './Core/Game'
 
-// dungeon 4
-import { create, history, Dungeon4Data, modulesAvailable } from './Generate/dungeon4/dungeon4'
-import { visualizer4, Visualizer4 } from './Generate/visualizer4'
-
-// Dungeon 4
-let visual4: Visualizer4
-let d4Data: Dungeon4Data | null
-const d4modules = mrModules()
-
 export function App() {
-  // messagge and main display
-  ;[msgDisplay, display] = createGameDisplay()
-  window.display = [display]
-
   // dev html background
   document.body.style.backgroundColor = CONFIG.htmlBGColor
 
   if (CONFIG?.seed) ROT.RNG.setSeed(CONFIG.seed)
 
-  switch (CONFIG.appInitial) {
-    case 'game':
-      startGame()
-      break
-    case 'dungeon4':
-      keys.add(inputD4)
-      startDungeon4()
-      break
-    default:
-      display.drawText(0, 0, 'Welcome to App! I have nothing to do. (' + CONFIG.appInitial + ')?')
-  }
-
-  function inputD4(key: string) {
-    switch (key) {
-      case 'KeyQ':
-        d4modules.next()
-        startDungeon4()
-        break
-      case 'KeyN':
-        startDungeon4()
-        break
-      case 'KeyP':
-        if (d4Data) startGame()
-        break
-      default:
-        visual4.control(key)
-    }
-  }
-
-  function startDungeon4() {
-    if (visual4 === undefined) visual4 = visualizer4(display, true, false, false)
-
-    console.log('d4modules.current:', d4modules.current())
-    try {
-      d4Data = create({ moduleRoomGen: d4modules.current() })
-    } catch (error) {
-      console.groupEnd()
-      console.groupEnd()
-      console.groupEnd()
-      console.groupEnd()
-      console.error(error)
-    } finally {
-      visual4.start(history, d4modules.current())
-    }
-  }
-
-  function startGame() {
-    if (keys) keys.cleanup()
-    if (visual4) visual4.cleanup()
-    window.game = new Game(display, msgDisplay)
-  }
-}
-
-// Module handler
-function mrModules() {
-  const modules = modulesAvailable
-  let index = 0
-
-  const current = () => {
-    return modules[index]
-  }
-
-  const next = () => {
-    index++
-    if (index > modules.length - 1) index = 0
-    return modules[index]
-  }
-
-  return { next, current }
+  const [msg, main] = createGameDisplay()
+  window.game = new Game(main, msg)
 }
 
 declare global {
   interface Window {
     game: object
-    display: ROT.Display[]
   }
 }
