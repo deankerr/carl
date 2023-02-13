5976a5aa07e523362debfcb16ce347d6e358868c
Dean Kerr <dean.kerr@gmail.com>
2023-02-14 05:39:29 +1100

    Overseer3 scaffolding

--- src/Generate/Overseer3.ts
+++ src/Generate/Overseer3.ts
@@ -1,5 +1,14 @@
 import { Region } from '../Core'
+import { Rect } from '../Model/Rectangle'
 
 export class Overseer3 {
-  constructor(readonly region: Region) {}
+  rect: Rect
+  pool = window.game.pool
+
+  timeStart = Date.now()
+  timeEnd = 0
+
+  constructor(readonly region: Region) {
+    this.rect = region.rect
+  }
 }
